a8113dbd3cb9a2258d50cc3d6c6cf742013b8130
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-10-31 20:59:05 -0500

    Add initial Dart "Hola Mundo" example

---
+++ 01_HOLA_MUNDO/RICHAR_CANGUI/CLASE/hola_mundo.dart
@@ -0,0 +1,11 @@
+/// Hola Mundo en Dart
+/// void -> no te retorna nada -> funciones
+/// main() -> función principal
+/// { -> bloque de la función - o cuerpo
+///
+/// }
+
+void main() {
+  /// print -> función u objeto que va a imprimir un valor en consola
+  print("Hola Mundo, me llamo Richar :D");
+}
