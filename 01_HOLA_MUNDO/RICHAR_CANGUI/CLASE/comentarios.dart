577aead18b783d2f15eb76f98d666ee12127ef46
Richar Cangui <richarsantiagocangui96@gmail.com>
2024-11-05 21:00:43 -0500

    Add comments and examples for Dart functions, documentation, and variable types

---
+++ 01_HOLA_MUNDO/RICHAR_CANGUI/CLASE/comentarios.dart
@@ -0,0 +1,39 @@
+void main() {
+  // Comentario simple 
+  // Windows -> ctrl + / 
+  // print("Hola mundo");
+  // print("Hola mundo");
+  // print("Hola mundo");
+
+  // Comentario de bloque
+  /*
+  print("Hola mundo");
+  print("Hola mundo");
+  print("Hola mundo");
+  */
+
+  // Shorcuts: 
+  // windows -> ctrl + / 
+  // windows -> ctrl + k + c -> crear comentario
+  // windows -> ctrl + k + u -> borrar comentario
+
+  // macos -> command + /
+
+
+
+  print("Hola");
+
+  /// -> un / adicional -> Comentario de documentación 
+  suma();
+
+}
+
+/// 
+/// # HOLA
+/// ## hola 2
+/// Esto es una documentación
+/// <h1> TEST </h1>
+/// 
+void suma() {
+  print(1+1);
+}
---
+++ 02_VARIABLES/RICHAR_CANGUI/CLASE/variables.dart
@@ -0,0 +1,36 @@
+// Esta es una variable global
+String global = "Este es un valor global";
+
+void main() {
+  // var
+  var nombre = "Richar";
+
+  // late
+  // inicializarlo mas tarde
+  late String edad;
+
+  edad = "12";
+
+  print(edad);
+
+  // Recomendable que siempre definas la variable con el tipo de dato
+  String apellido = "Cangui";
+
+
+  /// Const y Final 
+  /// 
+  /// Const -> se asigna su espacio de memoria al momento de compilar - 2s 
+  /// 
+  /// Final -> se asigna su espacio de memoria al momento de ejecutar - 2s
+  /// 
+  
+  const velocidadSonido = "250";
+
+  final diaSemana = "Lunes";
+
+
+
+
+
+
+}
