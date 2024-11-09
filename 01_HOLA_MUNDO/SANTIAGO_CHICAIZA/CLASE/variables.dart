8d23fe70396b028e9554f230b0b67e342f9e6709
santo-boot <santosantiago80@gmail.com>
2024-11-09 00:57:26 -0500

    Variables

---
+++ 01_HOLA_MUNDO/SANTIAGO_CHICAIZA/CLASE/variables.dart
@@ -0,0 +1,7 @@
+void main() {
+  late int numero;
+  var nombre = "Santiago";
+  numero = 88;
+  print(nombre);
+  print(numero);
+}
---
+++ 01_HOLA_MUNDO/SANTIAGO_CHICAIZA/mi_archivo.dart
@@ -0,0 +1,5 @@
+void main(){
+  // cuerpo de una funcion 
+print("Hola Mundo");
+
+}
